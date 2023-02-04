90debb676518377dcd3fa0b8f8e34573c54dff04
Amadi Jerry <amadijerry310@gmail.com>
2023-02-04 16:05:51 +0100

    impleted time count down

--- script.js
+++ script.js
@@ -10,39 +10,63 @@ let randomScrabbleWord = ''
 let rearrangedWord = ''
 let randomShuffleIndex = 0
 let correctWord = ''
+let defaultTime = 30
 
 
-function generateRandomScrabble(inputLength){
+window.addEventListener('load', () => {
+    reduceTime(defaultTime)
+})
+
+
+function generateRandomScrabble(inputLength) {
     randomScrabbleWord = Math.floor(Math.random() * wordsArr.length)
     hint.textContent = `Hint: ${wordsArr[randomScrabbleWord].hint}`
     correctWord = wordsArr[randomScrabbleWord].word
     inputLength.setAttribute('maxlength', correctWord.length)
     rearrangedWord = wordsArr[randomScrabbleWord].word.split('')
-    for(let i = 0; i < rearrangedWord.length; i++){
+    for (let i = 0; i < rearrangedWord.length; i++) {
         randomShuffleIndex = Math.floor(Math.random() * rearrangedWord.length)
         let swap = rearrangedWord[i]
         rearrangedWord[i] = rearrangedWord[randomShuffleIndex]
         rearrangedWord[randomShuffleIndex] = swap
     }
     scrabbledWord.textContent = rearrangedWord.join('').toUpperCase()
-    
+
 }
 
 
-function checkWord(){
-    if(scrabbleAnswer.value.toLowerCase() !== correctWord.toLowerCase()){
-        alert(`oops ${scrabbleAnswer.value} is not the correct word`)
-    }else{
+function checkWord() {
+    if (scrabbleAnswer.value.toLowerCase() !== correctWord.toLowerCase()) {
+        alert(`incorrect word`)
+    } else {
         alert(`congrats ${correctWord.toUpperCase()} is the correct word`)
     }
+    scrabbleAnswer.value = ''
 }
 
 checkBtn.addEventListener('click', () => {
     checkWord(scrabbleAnswer.value)
+    generateRandomScrabble(scrabbleAnswer)
+    defaultTime = 30
 })
 
 generateRandomScrabble(scrabbleAnswer)
 
-refreshBtn.addEventListener('click', () => {
+refreshBtn.addEventListener('click', refresh)
+
+function refresh(){
     generateRandomScrabble(scrabbleAnswer)
-})
+    scrabbleAnswer.value = ''
+    defaultTime = 30
+}
+
+function reduceTime() {
+    setTimeout(() => {
+        if (defaultTime < 0) {
+            alert(`Time up: ${correctWord.toUpperCase()} is the correct word`)
+            refresh()
+        }
+        time.textContent = `Time Left: ${defaultTime--}s`
+        reduceTime(defaultTime)
+    }, 1000)
+}
