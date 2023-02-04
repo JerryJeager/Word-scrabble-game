2f67ee0f82e6872f2cc05fd0d84dea30b7b30fa7
Amadi Jerry <amadijerry310@gmail.com>
2023-02-04 13:49:40 +0100

    added reshuffling scrabble words and checking for correctness

--- script.js
+++ script.js
@@ -1,2 +1,48 @@
 import { wordsArr } from "./words.js";
 
+const scrabbledWord = document.querySelector(".scrabbled-word")
+const hint = document.querySelector('.hint')
+const time = document.querySelector('.time')
+const scrabbleAnswer = document.querySelector('.answer')
+const refreshBtn = document.querySelector('.refresh-btn')
+const checkBtn = document.querySelector('.check-btn')
+let randomScrabbleWord = ''
+let rearrangedWord = ''
+let randomShuffleIndex = 0
+let correctWord = ''
+
+
+function generateRandomScrabble(inputLength){
+    randomScrabbleWord = Math.floor(Math.random() * wordsArr.length)
+    hint.textContent = `Hint: ${wordsArr[randomScrabbleWord].hint}`
+    correctWord = wordsArr[randomScrabbleWord].word
+    inputLength.setAttribute('maxlength', correctWord.length)
+    rearrangedWord = wordsArr[randomScrabbleWord].word.split('')
+    for(let i = 0; i < rearrangedWord.length; i++){
+        randomShuffleIndex = Math.floor(Math.random() * rearrangedWord.length)
+        let swap = rearrangedWord[i]
+        rearrangedWord[i] = rearrangedWord[randomShuffleIndex]
+        rearrangedWord[randomShuffleIndex] = swap
+    }
+    scrabbledWord.textContent = rearrangedWord.join('').toUpperCase()
+    
+}
+
+
+function checkWord(){
+    if(scrabbleAnswer.value.toLowerCase() !== correctWord.toLowerCase()){
+        alert(`oops ${scrabbleAnswer.value} is not the correct word`)
+    }else{
+        alert(`congrats ${correctWord.toUpperCase()} is the correct word`)
+    }
+}
+
+checkBtn.addEventListener('click', () => {
+    checkWord(scrabbleAnswer.value)
+})
+
+generateRandomScrabble(scrabbleAnswer)
+
+refreshBtn.addEventListener('click', () => {
+    generateRandomScrabble(scrabbleAnswer)
+})
--- words.js
+++ words.js
@@ -1,7 +1,51 @@
 var wordsArr = [
     {
-        word: "MESSI",
+        word: "Messi",
         hint: "Greatest footballer of all time"
+    },
+    {
+        word: "Canvas",
+        hint: "Piece of fabric for all painting"
+    },
+    {
+        word: "Minato",
+        hint: "The fourth hokage in Naruto"
+    },
+    {
+        word: "Sapa",
+        hint: "A state of no money"
+    },
+    {
+        word: "Chlorophyll",
+        hint: "Gives plant's green color"
+    },
+    {
+        word: "Poseidon",
+        hint: "Greek sea god"
+    }, 
+    {
+        word: "Aphrodite",
+        hint: "Greek goddess of love, sex and beauty"
+    },
+    {
+        word: "Anemia",
+        hint: "Insufficient red blood cells"
+    },
+    {
+        word: "Qatar",
+        hint: "Hosted the previous world cup"
+    },
+    {
+        word: "introvert",
+        hint: "A shy, reticent person"
+    },
+    {
+        word: "avocado",
+        hint: "a fruit"
+    },
+    {
+        word: "feather",
+        hint: "a hair like covering on birds"
     }
 ]
 
